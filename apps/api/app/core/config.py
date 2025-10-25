06629b50b0175b5581314a3d3f108a1d856f5f5a
Mahiamk <mahikomohammed@gmail.com>
2025-10-25 15:38:52 +0800

    feat: add configuration settings for environment variables using Pydantic

---
+++ apps/api/app/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import ClassVar
+class Settings(BaseSettings):
+    # This tells BaseSettings to look for an .env file
+    model_config = SettingsConfigDict(
+        env_file="../.env", # Path to your .env file
+        env_file_encoding='utf-8'
+    )
+    
+    # These variables MUST match the names in your .env file
+    SECRET_KEY: str = "your_default_secret_if_env_is_missing"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 300
+    SUPERUSER_EMAILS: ClassVar[list[str]] = ["admin@gmail.com"]
+
+    
+    # DATABASE_URL is already loaded in database.py, but
+    # it's good practice to have all env settings here.
+    DATABASE_URL: str
+
+# Create a single, importable instance of your settings
+settings = Settings()
